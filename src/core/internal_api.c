beae17e53b6c92b9989a5b746232270a0c50ec2f
hsfk <hsfk@yandex.ru>
2016-07-17 21:38:55 +1000

    add return and cast

--- src/core/internal_api.c
+++ src/core/internal_api.c
@@ -2,28 +2,31 @@
 
 #include <Windows.h>
 
+#include "..\..\include\errors.h"
 #include "..\..\include\external_api.h"
 
 __VM_STD_INSTR(load_lib)
 {
     char* lib_name = &mem->cells[pop32(__VM_STD_INSTR_ARGS)];
     HMODULE lib = LoadLibraryA(lib_name);
-    WREG(0) = lib;
+    WREG(0) = (WORD)lib;
+    return NO_ERR;
 }
 
 __VM_STD_INSTR(get_proc_addr)
 {
-    HMODULE lib = pop32(__VM_STD_INSTR_ARGS);
+    HMODULE lib = (DWORD)pop32(__VM_STD_INSTR_ARGS);
     if (!lib)
     {
-        WREG(0) = NULL;
-        return;
+        WREG(0) = (WORD)NULL;
+        return NO_ERR;
     }
     char* proc_name = &mem->cells[pop32(__VM_STD_INSTR_ARGS)];
-    WREG(0) = GetProcAddress(lib, proc_name);
+    WREG(0) = (WORD)GetProcAddress(lib, proc_name);
+    return NO_ERR;
 }
 
 __VM_STD_INSTR(invoke)
 {
-
+    return NO_ERR;
 }
